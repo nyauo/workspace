b6426d16d90d546fda9a0a3c9e48d5190c86413f
魚ちゃん食べる <1391502113@qq.com>
2025-07-20 16:28:48 +0800

    Update calculateCurrents.m

--- calculateCurrents.m
+++ calculateCurrents.m
@@ -6,10 +6,11 @@ function currents = calculateCurrents(V, x, config)
     V_drop = V - currents.total .* x(2);
     
     % 计算各分量电流
-    currents.diode = x(1) * (exp(config.physics.A * V_drop / config.physics.n) - 1);
+       currents.diode = x(1) * (exp(config.physics.A * V_drop / config.physics.n) - 1);
+    currents.tunnel = x(5) * (exp(config.physics.A * V_drop / config.physics.n2) - 1);
     currents.ohmic = V_drop / x(3);
     currents.nonohmic = x(4) * (abs(V_drop).^config.physics.m) .* sign(V_drop);
-    
+
     % 计算总电流（验证用）
-    currents.sum = currents.diode + currents.ohmic + currents.nonohmic;
+    currents.sum = currents.diode + currents.tunnel + currents.ohmic + currents.nonohmic;
 end

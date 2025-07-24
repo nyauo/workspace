07c893bb1661eed2b950a446eda227f141460f4b
魚ちゃん食べる <1391502113@qq.com>
2025-07-24 18:40:19 +0800

    Update loadConfig.m
    增加n2步长和范围

--- io/loadConfig.m
+++ io/loadConfig.m
@@ -24,7 +24,9 @@ function config = loadConfig()
         'improvement_threshold', 0.1, ... % 最小改进幅度 (百分比)
         'termination_avg_error', 5, ...   % 平均误差阈值，低于此值则提前结束
         'm_range', [2, 3], ...     % 调整m的范围
-        'm_step', 0.1 ...          % 调整m的步长
+        'm_step', 0.1, ...         % 调整m的步长
+        'n2_range', [2, 8], ...  % 调整n2的范围
+        'n2_step', 0.1 ...         % 调整n2的步长
     );
     % Regularization settings for parameter priors
     config.regularization = struct(...
